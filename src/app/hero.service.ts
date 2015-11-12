6e95bcba430a423cf2dc2d7faf43934cee2c243e
Andre <andy@andres-mbp.localdomain>
2015-11-12 22:08:02 +0100

    heroes mock extended

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -7,8 +7,16 @@ export class HeroService{
 }
 
 var HEROES: Hero[] = [
-	{ "id": 1, "name": "Superman"},
-	{ "id": 2, "name": "Spiderman"},
-	{ "id": 3, "name": "Wolferine"},
-	{ "id": 4, "name": "Hulk"}
+	{ "id": 1, "name": "Hulk"},
+	{ "id": 2, "name": "Wolferine"},
+	{ "id": 3, "name": "War Machine"},
+	{ "id": 4, "name": "Mangneto"},
+	{ "id": 5, "name": "Ant-Man"},
+	{ "id": 6, "name": "Thor"},
+	{ "id": 7, "name": "Captain America"},
+	{ "id": 8, "name": "Batman"},
+	{ "id": 9, "name": "Storm"},
+	{ "id": 10, "name": "Subzero"},
+	{ "id": 11, "name": "Iron Man"},
+	{ "id": 12, "name": "Superman"},
 ];
