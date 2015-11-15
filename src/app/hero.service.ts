ed3480a611bd762e9c637e70352f293bd2c8317b
Andre <andy@andres-mbp.localdomain>
2015-11-15 20:23:24 +0100

    firebase introduced in service

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,16 +1,31 @@
+/// <reference path="../../typings/firebase/firebase.d.ts" />
+
 import {Hero} from './hero';
 
+const FIREBASE_URL = 'https://radiant-torch-9235.firebaseio.com';
+
 export class HeroService{
+	firebase : Firebase;
+	heroList : Hero[];
+
+	constructor(){
+		this.firebase = new Firebase(FIREBASE_URL);
+		this.heroList = [];
+
+		this.firebase.on('child_added',
+			snapshot => this.addHeros(snapshot),
+			errorObject => console.log('The read failed', errorObject.code)
+		);
+	}
+
+	private addHeros(snapshot: FirebaseDataSnapshot) {
+		var heroes = snapshot.val();
+		for(var i in heroes){
+			this.heroList.push(heroes[i]);
+		}
+	}
+
 	getHeroes() {
-		return Promise.resolve(HEROES);
+		return Promise.resolve(this.heroList);
 	}
 }
-
-var HEROES: Hero[] = [
-	{ "id": 1, "name": "Hulk"},
-	{ "id": 2, "name": "Wolferine"},
-	{ "id": 3, "name": "War Machine"},
-	{ "id": 4, "name": "Mangneto"},
-	{ "id": 5, "name": "Ant-Man"},
-	{ "id": 6, "name": "Thor"}
-];
