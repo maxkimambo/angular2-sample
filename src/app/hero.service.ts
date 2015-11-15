98a16fa920884f4fa2159cff44e9f2e7e75a6671
Andre <andy@andres-mbp.localdomain>
2015-11-15 15:08:30 +0100

    service mock changed

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -12,11 +12,5 @@ var HEROES: Hero[] = [
 	{ "id": 3, "name": "War Machine"},
 	{ "id": 4, "name": "Mangneto"},
 	{ "id": 5, "name": "Ant-Man"},
-	{ "id": 6, "name": "Thor"},
-	{ "id": 7, "name": "Captain America"},
-	{ "id": 8, "name": "Batman"},
-	{ "id": 9, "name": "Storm"},
-	{ "id": 10, "name": "Subzero"},
-	{ "id": 11, "name": "Iron Man"},
-	{ "id": 12, "name": "Superman"},
+	{ "id": 6, "name": "Thor"}
 ];
